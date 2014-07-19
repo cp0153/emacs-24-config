32e6b4f207f8bd7e6c62f94d1e3fafc7259af1d8
cp0153 <christopher.hale.pearce@gmail.com>
2014-07-19 14:02:35 -0400

    downloaded from wrong github repo Revert "solarized added but doesnt load"
    This reverts commit 88d84b54fdacf9f883f927fb5ccf3a6cf0deeeda.

--- init.el
+++ init.el
@@ -12,17 +12,15 @@
      ; entry is introduced by a line beginning with
      ; three semicolons.
 
-;manually added custom themes zenburn and solarized
+;added custom themes
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/solarized-emacs/manual_installation")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "3c9d994e18db86ae397d077b6324bfdc445ecc7dc81bb9d528cd9bba08c1dac1" default))))
+ '(custom-safe-themes (quote ("3c9d994e18db86ae397d077b6324bfdc445ecc7dc81bb9d528cd9bba08c1dac1" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
