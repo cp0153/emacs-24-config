f07ad2424772ecaec81eb2e287f4b1e85440a656
cp0153 <christopher.hale.pearce@gmail.com>
2014-07-20 10:07:04 -0400

    set emacs to save buffers on exit

--- init.el
+++ init.el
@@ -59,3 +59,13 @@
 )
 (toggle-fullscreen)
 
+;set emacs to save buffers on exit
+(require 'desktop)
+  (desktop-save-mode 1)
+  (defun my-desktop-save ()
+    (interactive)
+    ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
+    (if (eq (desktop-owner) (emacs-pid))
+        (desktop-save desktop-dirname)))
+  (add-hook 'auto-save-hook 'my-desktop-save)
+
